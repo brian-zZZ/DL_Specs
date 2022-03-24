04e2fd08b0a4e6333dd44b2aae508a9656e488df
Brian Zhang <1129291361@qq.com>
2022-03-24 17:03:22 +0800

    Add files via upload

--- Graphs/GCN/README.md
+++ Graphs/GCN/README.md
@@ -1,1 +1,20 @@
+# GCNs with Fast Approximate Convolutions
+Pytorch implementation of paper [SEMI-SUPERVISED CLASSIFICATION WITH GRAPH CONVOLUTIONAL NETWORKS](http://arxiv.org/abs/1609.02907)
 
+## Key contributions
+Introduce a novel **fast approximate convolutions** illustrated below.
+
+<p align="center">
+<img src="key_contributions.png" width="250">
+</p>
+
+## Usage
+```bash
+python train.py
+```
+
+## References
+* [Offical Pytorch implementation of the paper](https://github.com/tkipf/gcn).
+* [GCNs Blog](http://tkipf.github.io/graph-convolutional-networks/) by Thomas N. Kipf.
+* Distill paper: [A Gentle Introduction to Graph Neural Networks](https://distill.pub/2021/gnn-intro/), which provides a general view for GNNs.
+* Distill paper: [Understanding Convolutions on Graphs](https://distill.pub/2021/understanding-gnns/), which briefly introduce the underlying mechanism of GNNs.
---
+++ Graphs/GCN/utils.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+import torch
+
+
+def load_data(path="./data/cora/", dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(dataset))
+
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset),
+                                        dtype=np.dtype(str))
+    '''csr_matrix方法将输入matrix转换到CSR(Compressed Sparse Row matrix)格式
+    Advantages of the CSR format
+      - efficient arithmetic operations CSR + CSR, CSR * CSR, etc.
+      - efficient row slicing
+      - fast matrix vector products
+    '''
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = pd.get_dummies(idx_features_labels[:, -1]).values
+
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset),
+                                    dtype=np.int32)
+    # 将cora.cites由<ID of cited paper> <ID of citing paper>的ID匹配到cora.content中的ID
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]),
+                        dtype=np.float32) # A sparse matrix in COOrdinate format
+
+    # build symmetric adjacency matrix 对于在对称位置的两点，取大的值作为二者共同的取值
+    # 实现上，对于转置后更大的点取更大的值(adj.T+adj-adj)，对于转置后更小的点不改动(adj+0-0)
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0])) # adj with added self-connections
+
+    idx_train = range(140)
+    idx_val = range(200, 500)
+    idx_test = range(500, 1500)
+
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(np.where(labels)[1])
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
